b074aa96b276b130bfee571551245a0c2e2cc5c6
Rasyid Prayogo <rasyidp84@gmail.com>
2026-01-20 23:52:00 +0700

    Dekorasi TextField

--- lib/views/FiturTextField.dart
+++ lib/views/FiturTextField.dart
@@ -14,15 +14,62 @@ class FiturTextField extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.all(20),
             child: TextField(
+              showCursor: true,
+              cursorColor: Colors.red,
+              textAlign: TextAlign.center,
+              cursorWidth: 10,
+              cursorHeight: 25,
+              cursorRadius: Radius.circular(20),
+              textAlignVertical: TextAlignVertical.center,
+              textCapitalization: TextCapitalization.characters,
               autocorrect: true,
               autofocus: true,
               enableSuggestions: true,
               enableInteractiveSelection: true,
-              obscureText: true,
-              obscuringCharacter: "-",
+              // obscureText: true,
+              // obscuringCharacter: "-",
               keyboardType: TextInputType.number,
-              readOnly: true,
+              readOnly: false,
               // enabled: false,
+
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 18,
+                // fontWeight: FontWeight.bold,
+              ),
+
+              decoration: InputDecoration(
+                icon: Icon(Icons.person, size: 35,),
+                border: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.green,
+                    width: 2,
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey,
+                    width: 2,
+                  ),
+                ),
+                // prefixIcon: Icon(
+                //   Icons.person_add,
+                //   size: 35,
+                // ),
+                // prefixText: "Nama : ",
+                // prefix: Icon(Icons.person, size: 35,)\
+                hintText: "Masukkan Nama Anda",
+                hintStyle: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 14,
+                ),
+                labelText: "Nama Lengkap",
+                labelStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                ),
+              ),
             ),
           ),
         ),
